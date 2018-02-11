0275074b94f89c9fc2b95f3a20c2438beefeb64c
rja907 <kmrrajj@gmail.com>
2018-02-11 23:54:00 +0530

    Day 4

---
+++ day4classes.js
@@ -0,0 +1,22 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+    function Polygon (arr){
+        this.perimeter = function() {
+            var total = 0;
+            for(let i = 0; i < arr.length; i++){
+            total = total + arr[i];
+        }
+        return total;
+        };
+
+    }
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
