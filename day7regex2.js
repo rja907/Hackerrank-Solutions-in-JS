ffd5fb44fc0617afdf542dbacccc9adcceba4bf4
rja907 <kmrrajj@gmail.com>
2018-02-12 12:12:17 +0530

    Day 7

---
+++ day7regex2.js
@@ -0,0 +1,14 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
+     * followed by one or more letters.
+     */
+
+    //let re = /^(Mr\.|Mrs\.|Ms\.|Dr.|Er\.).*/;
+    let re = /^(Mr|Mrs|Ms|Dr|Er)\.\w+$/;
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
